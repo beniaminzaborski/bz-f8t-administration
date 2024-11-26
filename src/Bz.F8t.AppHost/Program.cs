9036e82bde71258549f121de26a71f30ddbf3340
Beniamin Zaborski <beniamin.zaborski@gmail.com>
2024-11-26 17:57:09 +0100

    Add Aspire hosting dependencies

--- src/Bz.F8t.AppHost/Program.cs
+++ src/Bz.F8t.AppHost/Program.cs
@@ -1,5 +1,17 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Bz_F8t_Administration_WebAPI>("f8t-admin");
+var postgres = builder.AddPostgres("f8t-postgres");
+
+var appInsights = builder.AddAzureApplicationInsights("f8t-appinsights");
+
+var serviceBus = builder.AddAzureServiceBus("f8t-servicebus");
+
+builder.AddProject<Projects.Bz_F8t_Administration_WebAPI>("f8t-admin")
+    .WithReference(postgres)
+    .WaitFor(postgres)
+    .WithReference(appInsights)
+    .WaitFor(appInsights)
+    .WithReference(serviceBus)
+    .WaitFor(serviceBus);
 
 builder.Build().Run();
